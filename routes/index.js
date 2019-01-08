621c3b69a0b22d06e700816c599986fcc6a6e207
MawiraIke <mawiraike@gmail.com>
2019-01-08 14:04:06 +0300

    Full backend

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,67 @@ router.get('/', (req, res) => {
 
 router.get('/info', (req, res) => {
     res.render('info', {
-        data
+        data: data,
+        count: data.length
     });
+});
+
+router.get('/fare', (req, res) => {
+    res.render('fare', {
+        data: data,
+        error: null,
+        count: data.length
+    })
+});
+
+router.post('/compare', (req, res) => {
+    var from;
+    var to;
+    var customData = [];
+    var count = 0;
+    if (req.body.from == '' && req.body.to == '') {
+        res.render('fare', {
+            data: data,
+            error: "One field must be filled, both cannot be empty",
+            count: null
+        });
+        return;
+    } else if (req.body.from == '' && req.body.to !== '') {
+        to = req.body.to;
+        from = 'Anywhere';
+    } else if (req.body.to == '' && req.body.from !== '') {
+        from = req.body.from;
+        to = 'Anywhere'
+    } else {
+        from = req.body.from;
+        to = req.body.to;
+    }
+    for (var i = 0; i < data.length; i++) {
+        if (from == 'Anywhere') {
+            if (data[i]["To"] == to) {
+                customData.push(data[i]);
+                count++;
+            }
+        } else if (to == 'Anywhere') {
+            if (data[i]["From"] == from) {
+                customData.push(data[i]);
+                count++;
+            }
+        } else {
+            if (data[i]["From"] == from && data[i]["To"] == to) {
+                customData.push(data[i]);
+                count++;
+            }
+        }
+    }
+    res.render('fare', {
+        data: customData,
+        error: null,
+        count: count
+    })
+
+
+
 });
 
 router.get("*", (req, res) => {
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const routes = require('./routes');
 const path = require('path');
+const bodyParser = require('body-parser')
 
 const port = process.env.port || 3000;
 
@@ -9,6 +10,19 @@ app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.static('public'));
 
 app.set("view engine", "ejs");
+
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
+ */
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
 app.use('/', routes);
 
 app.listen(port, () => {
